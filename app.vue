fab4e17afde38c21e299cc1494ed10365047ea0c
Germain-D <germain.deffontaines@efrei.net>
2025-06-07 12:33:39 +0200

    update title

--- app.vue
+++ app.vue
@@ -2,30 +2,31 @@
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
-
 </template>
 
 <script setup>
 useHead({
-  title: "GribbouilleCourse",
+  title: "GribouilleCourse",
   meta: [
     {
       name: "description",
       content:
-        "GribouilleCourse est une application web qui vous permet de dessiner un parcours de course à pied, de le générer en fichier GPX et de le suivre avec votre montre GPS ou votre smartphone."
+        "GribouilleCourse est une application web qui vous permet de dessiner un parcours de course à pied, de le générer en fichier GPX et de le suivre avec votre montre GPS ou votre smartphone.",
     },
   ],
 });
-
-
 </script>
 
 <style>
-h1, h2, h3, .card-title, .footer-title, .btn {
-  font-family: 'Roboto Condensed', sans-serif;
+h1,
+h2,
+h3,
+.card-title,
+.footer-title,
+.btn {
+  font-family: "Roboto Condensed", sans-serif;
   font-weight: 700;
   letter-spacing: -0.5px;
   text-transform: uppercase;
 }
-
 </style>
